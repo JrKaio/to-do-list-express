76c9bb79cbd7430a96766d272f529a8380367726
JrKaio <kaiojunio22@hotmail.com>
2023-01-09 15:28:05 -0300

    Utilizando o post

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const checkListRouter = require ('./src/routes/checklist')
-const app = express ();
+const checkListRouter = require ('./src/routes/checklist');
 
+const app = express ();
 app.use(express.json())
-app.use(checkListRouter)
+
+app.use("/checklists", checkListRouter)
 
 app.listen(3000, () =>{
     console.log('Servidor foi iniciado');
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,8 +1,13 @@
 const express = require ('express')
 const router = express.Router()
 
-router.get('checklists', (req, res)=>{
+router.get('/', (req, res)=>{
     console.log('Olá');
     res.send();
 }) // Criou uma nova rota
 module.exports = router;
+
+router.post('/', (req, res)=>{
+    console.log(req.body); // Esse mostra no terminal
+     res.status(200).json(req.body) // Esse mostra no postman
+})
