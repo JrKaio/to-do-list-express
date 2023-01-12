559ffb61194da926adf6dcd4c656791fd4ff7d38
JrKaio <kaiojunio22@hotmail.com>
2023-01-12 19:26:19 -0300

    Inicializando com viewpage

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -5,9 +5,9 @@ const Checklist = require('../models/checklist')
 router.get('/', async (req, res) =>{
 try{
     let checklists = await Checklist.find({});
-    res.status(200).json(checklists)
+    res.status(200).render('checklists/index', {checklists: checklists })
 } catch (error) {
-    res.status(500).json(error)
+    res.status(200).render('pages/error', {erro: 'Erro ao exibir as Listas' })
 
 }// Le tudo do banco de dados
 
@@ -25,9 +25,26 @@ router.post ('/', async (req, res) =>{
 router.get('/:id', async (req, res) =>{
     try {
         let checklist = await Checklist.findById(req.params.id)
+        res.status(200).render('checklists/show', {checklist: checklist })
+    } catch (error) {
+        res.status(200).render('pages/error', {erro: 'Erro ao exibir as Listas' })
+    } // Retorna o resultado via ID no postman
+})
+router.put('/:id', async (req, res)=>{
+    let { name } = req.body
+    try {
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new : true}) // Atualizar algo, no caso será o --> name
         res.status(200).json(checklist);
     } catch (error) {
         res.status(422).json(error);
-    } // Retorna o resultado via ID no postman
+    }
+})
+router.delete ('/:id', async (req, res)=>{
+    try {
+        let checklist = await Checklist.findByIdAndDelete(req.params.id) // Atualizar algo, no caso será o --> name
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 })
 module.exports = router;
