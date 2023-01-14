acfd68f6aebdba5f5cf0d667434406bb31ef5dd8
JrKaio <kaiojunio22@hotmail.com>
2023-01-14 13:31:05 -0300

    Editando e removendo listas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,5 +1,4 @@
 const express = require ('express');
-const checklist = require('../models/checklist');
 const router = express.Router()
 const Checklist = require('../models/checklist')
 
@@ -18,8 +17,16 @@ router.get('/new', async (req, res) =>{
         let checklist = new Checklist()
         res.status(200).render('checklists/new', {checklist: checklist })
     } catch (error) {
-        res.status(500).render('pages/error', {errors: 'Erro ao carregar formulário' })
+        res.status(500).render('pages/error', {error: 'Erro ao exibir a edição de lista de tarefas' })
     } 
+})
+router.get('/:id/edit', async(req, res) =>{
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).render('checklists/edit', {checklist: checklist})
+    } catch (error) {
+        res.status(200).render('pages/error', {error: 'Erro ao exibir as Listas' })
+    }
 })
 
 router.post ('/', async (req, res) =>{
@@ -43,20 +50,22 @@ router.get('/:id', async (req, res) =>{
 })
 
 router.put('/:id', async (req, res)=>{
-    let { name } = req.body
+    let { name } = req.body.checklist;
+    let checklist = await Checklist.findById(req.params.id)
     try {
-        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new : true}) // Atualizar algo, no caso será o --> name
-        res.status(200).json(checklist);
+        await checklist.update({name})  // Atualizar algo, no caso será o --> name
+        res.redirect('/checklists')
     } catch (error) {
-        res.status(422).json(error);
+        let errors = error.errors
+        res.status(422).render('checklists/edit', {checklist: {...checklist, errors}})
     }
 })
 router.delete ('/:id', async (req, res)=>{
     try {
         let checklist = await Checklist.findByIdAndDelete(req.params.id) // Atualizar algo, no caso será o --> name
-        res.status(200).json(checklist);
+        res.redirect('/checklists')
     } catch (error) {
-        res.status(422).json(error);
+        res.status(500).render('pages/error', {error: 'Erro ao deletar a lista de tarefas'})
     }
 })
 module.exports = router;
